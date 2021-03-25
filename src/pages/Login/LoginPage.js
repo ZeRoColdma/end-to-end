ae6958a11399c6e67beb1626ad98f6d946b0d26c
Carlos Silva <ccarloos9@gmail.com>
2021-03-24 21:50:13 -0300

    Responsividade Ok

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 import "./LoginPage.css";
-import Sidebar from "../../components/sidebar";
+import logoEndToEnd from "../../assets/endtoend.png";
 
 export default function LoginPage({ history }) {
   const [login, setLogin] = useState("");
@@ -16,37 +16,58 @@ export default function LoginPage({ history }) {
 
   return (
     <>
-      <Sidebar />
-      <div id="page-landing" className="container-fluid">
-        <Form onSubmit={handleSubmit}>
-          <Form.Text className="text-muted">
-            <h1>Olá</h1>
-            <h4>Acesse aqui seus dashboards!</h4>
-          </Form.Text>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Seu Email"
-              value={login}
-              onChange={(event) => setLogin(event.target.value)}
-            />
-          </Form.Group>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-4">
+            <aside className="app-sidebar">
+              <img src={logoEndToEnd} alt="Logo End To End" />
+              <div>
+                <h1>
+                  Inteligência para o seu <br />
+                  negócio
+                </h1>
+                <p>
+                  Ajudamos você a tomar decisões e para <br />
+                  aprimorar a gestão da sua empresa e melhorar seus resultados
+                </p>
+              </div>
+            </aside>
+          </div>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Senha</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Senha"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </Form.Group>
+          <div id="page-landing" className="col-md-12 col-lg-8 row">
+            <div className="container-fluid">
+              <Form onSubmit={handleSubmit}>
+                <Form.Text className="text-muted">
+                  <h1>Olá</h1>
+                  <h4>Acesse aqui seus dashboards!</h4>
+                </Form.Text>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Seu Email"
+                    value={login}
+                    onChange={(event) => setLogin(event.target.value)}
+                  />
+                </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Entrar
-          </Button>
-        </Form>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Entrar
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
